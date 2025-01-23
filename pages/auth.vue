2c1c9bf1411161e6cabb30e8025bb88237f45180
Aboba074 <dfa.asdasda@mail.ru>
2025-01-23 19:31:42 +0500

    Update auth.vue

--- pages/auth.vue
+++ pages/auth.vue
@@ -41,25 +41,28 @@
     const router = useRouter()
 
 
-    /* вход */
-    const authUser = async() => {  
-        const { data: users, error } = await supabase
-        .from('users')
-        .select("*")
-        .eq('login', user.value.login)
-    
-        if (!users[0]) {
-            user.value.login = ""
-            return showMessage("Неверно введен логин!", false)              
-        }
+  /* вход */
+const { decrypt } = useCryptStore()
+const authUser = async() => {
+  const { data: users, error } = await supabase
+    .from('users')
+    .select("*")
+    .eq('login', user.value.login)
 
-        if (user.value.password !== users[0].password) {
-            user.value.password = ""
-            return showMessage('Неверно введен пароль!', false)            
-        }
+  if (!users[0]) {
+    user.value.login = ""
+    return showMessage("Неверно введен логин!", false)
+  }
 
-        showMessage('Успешный вход!', true)
-        login(users[0].id, users[0].role)
-        router.push('/profile')
-    } 
+  users[0].password = await decrypt(users[0].password)
+
+  if (user.value.password !== users[0].password) {
+    user.value.password = ""
+    return showMessage('Неверно введен пароль!', false)
+  }
+
+  showMessage('Успешный вход!', true)
+  login(users[0].id, users[0].role)
+  router.push('/profile')
+}
 </script>
